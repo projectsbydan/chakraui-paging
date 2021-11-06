aede2dc8acdac0940107a31a02d8ebe629a21217
Dan Deaconu <36627723+projectsbydan@users.noreply.github.com>
2021-11-06 10:37:55 +0100

    finished demo

--- paging-demo/src/App.tsx
+++ paging-demo/src/App.tsx
@@ -6,6 +6,13 @@ import {
   VStack,
   Grid,
   theme,
+  NumberInput,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInputField,
+  NumberInputStepper,
+  StackItem,
+  Button,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { PagingControls } from "./components/pagingControls"
@@ -26,6 +33,9 @@ export const App = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(3);
 
+  const addMoreItems = () => {
+    setItems([...items, ...generateRandomItems()]);
+  }
 
   return <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
@@ -36,13 +46,36 @@ export const App = () => {
           <Text fontSize="3xl" >
             Chakra UI Paging Demo
           </Text>
-          {items.slice((currentPage-1) * pageSize, (currentPage)*pageSize).map(x => <Box key={x.id}>{x.text}</Box>)}
+
+          <Text fontSize="2xl" fontWeight="semibold">Controls</Text>
+          <StackItem>
+            <Text>currentPage:</Text>
+            <NumberInput type="number" value={currentPage} onChange={(value) => setCurrentPage(Number(value ?? 0))}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </StackItem>
+          <StackItem>
+            <Text>pageSize: </Text><NumberInput type="number" value={pageSize} onChange={(value) => setPageSize(Number(value ?? 0))}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </StackItem>
+          <Button onClick={addMoreItems}>Add more items</Button>
+          <Box mb="2em"></Box>
+          {items.slice((currentPage - 1) * pageSize, (currentPage) * pageSize).map(x => <Box key={x.id}>{x.text}</Box>)}
           <PagingControls currentPage={currentPage} onGoToPage={(newPageNumber) => setCurrentPage(newPageNumber)} pageSize={pageSize} totalCount={items.length} totalCountDescription={"items avaialable"} />
           <Box mt="2em" textAlign="center">
-          <Text fontSize="2xl" fontWeight="semibold">Debugging</Text>
-          <Text>currentPage: {currentPage}</Text>
-          <Text>pageSize: {pageSize}</Text>
-        </Box>
+            <Text fontSize="2xl" fontWeight="semibold">Debugging</Text>
+            <Text>currentPage: {currentPage}</Text>
+            <Text>pageSize: {pageSize}</Text>
+          </Box>
         </VStack>
       </Grid>
     </Box>
--- paging-demo/src/components/pagingControls.tsx
+++ paging-demo/src/components/pagingControls.tsx
@@ -1,85 +1,135 @@
 import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
-import { Button, HStack, IconButton, Stack, Text } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Button,
+  HStack,
+  IconButton,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
 interface IPagingControls {
-    totalCount: number;
-    totalCountDescription: string;
-    pageSize: number;
-    onGoToPage: (pageNumber: number) => void;
-    currentPage: number;
+  totalCount: number;
+  totalCountDescription: string;
+  pageSize: number;
+  onGoToPage: (pageNumber: number) => void;
+  currentPage: number;
 }
 
 const getPageNumbers = (
-    currentPage: number,
-    totalCount: number,
-    pageSize: number,
-    currentPages: number[]
+  currentPage: number,
+  totalCount: number,
+  pageSize: number,
+  currentPages: number[]
 ) => {
-    if (currentPages.indexOf(currentPage) !== -1) {
-        return currentPages;
+  if (currentPages.indexOf(currentPage) !== -1) {
+    return currentPages;
+  }
+
+  const maxNumber = Math.ceil(totalCount / pageSize);
+  const firstPagePosition = Math.min(currentPage, maxNumber - 4);
+
+  const result: number[] = [];
+  for (
+    let i = firstPagePosition - 1;
+    i < firstPagePosition + 4 && i < maxNumber;
+    i++
+  ) {
+    if (i < 0) {
+      continue;
     }
 
-    const maxNumber = Math.ceil(totalCount / pageSize);
+    result.push(i + 1);
+  }
 
-    const firstPagePosition = Math.min(currentPage, maxNumber - 4);
-
-    const result: number[] = [];
-    for (let i = firstPagePosition - 1; i < firstPagePosition + 4 && i < maxNumber; i++) {
-        if (i < 0) {
-            continue;
-        }
-
-        result.push(i + 1);
-    }
-
-    return result;
+  return result;
 };
 
 export const PagingControls = (props: IPagingControls) => {
-    const [pages, setPages] = useState(getPageNumbers(
-        props.currentPage,
-        props.totalCount,
-        props.pageSize, []
-    ));
+  const [pages, setPages] = useState(
+    getPageNumbers(props.currentPage, props.totalCount, props.pageSize, [])
+  );
 
-    useEffect(() => {
-        setPages(getPageNumbers(
-            props.currentPage,
-            props.totalCount,
-            props.pageSize, pages));
-    }, [props.currentPage]);
-
-    return (
-        <Stack align="center">
-            {pages.length > 1 && (
-                <HStack>
-                    <IconButton
-                        disabled={props.currentPage - 1 <= 0}
-                        aria-label="previous"
-                        onClick={() => props.onGoToPage(props.currentPage - 1)}
-                        icon={<ArrowBackIcon />}
-                    ></IconButton>
-                    {pages.map((pageNumber) => (
-                        <Button
-                            key={pageNumber}
-                            onClick={() => props.onGoToPage(pageNumber)}
-                            colorScheme={pageNumber === props.currentPage ? "teal" : undefined}
-                        >
-                            {pageNumber}
-                        </Button>
-                    ))}
-                    <IconButton
-                        aria-label="next"
-                        disabled={Math.max(...pages) +1 > props.totalCount / props.pageSize}
-                        onClick={() => props.onGoToPage(props.currentPage + 1)}
-                        icon={<ArrowForwardIcon />}
-                    ></IconButton>
-                </HStack>
-            )}
-            <Text fontWeight="extrabold">
-                {props.totalCount} {props.totalCountDescription}
-            </Text>
-        </Stack>
+  useEffect(() => {
+    setPages(
+      getPageNumbers(props.currentPage, props.totalCount, props.pageSize, [])
     );
+  }, [props.pageSize, props.totalCount]);
+
+  useEffect(() => {
+    setPages(
+      getPageNumbers(props.currentPage, props.totalCount, props.pageSize, pages)
+    );
+  }, [props.currentPage]);
+
+  if (props.currentPage < 1) {
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle>The current page index is invalid</AlertTitle>
+        <AlertDescription>
+          The current page index cannot be lower than 1. It currently is '
+          {props.currentPage}'
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (
+    props.currentPage > props.totalCount / props.pageSize &&
+    props.totalCount > props.pageSize
+  ) {
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle>The current page index is invalid</AlertTitle>
+        <AlertDescription>
+          The current page index cannot be greated than maximum number of pages
+          '{Math.floor(props.totalCount / props.pageSize)}'. It currently is '
+          {props.currentPage}'
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  return (
+    <Stack align="center">
+      {pages.length > 1 && (
+        <HStack>
+          <IconButton
+            disabled={props.currentPage - 1 <= 0}
+            aria-label="previous"
+            onClick={() => props.onGoToPage(props.currentPage - 1)}
+            icon={<ArrowBackIcon />}
+          ></IconButton>
+          {pages.map((pageNumber) => (
+            <Button
+              key={pageNumber}
+              onClick={() => props.onGoToPage(pageNumber)}
+              colorScheme={
+                pageNumber === props.currentPage ? "teal" : undefined
+              }
+            >
+              {pageNumber}
+            </Button>
+          ))}
+          <IconButton
+            aria-label="next"
+            disabled={
+              Math.max(...pages) + 1 > props.totalCount / props.pageSize
+            }
+            onClick={() => props.onGoToPage(props.currentPage + 1)}
+            icon={<ArrowForwardIcon />}
+          ></IconButton>
+        </HStack>
+      )}
+      <Text fontWeight="extrabold">
+        {props.totalCount} {props.totalCountDescription}
+      </Text>
+    </Stack>
+  );
 };
